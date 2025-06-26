e8d82e012005c2a9963014e4b07f3c51447a1aea
tiennm99 <tiennm99@outlook.com>
2025-06-26 23:22:28 +0700

    feat(svelte): migrate to svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { onMount } from 'svelte';
+  import { format, subDays } from 'date-fns';
+  import ExchangeRateViewer from '$lib/ExchangeRateViewer.svelte';
+  
+  let defaultStartDate = '';
+  let defaultEndDate = '';
+  
+  onMount(() => {
+    const today = new Date();
+    const weekAgo = subDays(today, 7);
+    
+    defaultStartDate = format(weekAgo, 'yyyy-MM-dd');
+    defaultEndDate = format(today, 'yyyy-MM-dd');
+  });
+</script>
+
+<main>
+  {#if defaultStartDate && defaultEndDate}
+    <ExchangeRateViewer 
+      defaultStartDate={defaultStartDate} 
+      defaultEndDate={defaultEndDate} 
+    />
+  {/if}
+</main>
